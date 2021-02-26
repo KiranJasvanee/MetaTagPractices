adeb72fea0ec21b43b17102b7c375c3c04825cee
Kiran Jasvanee <kiran.jasvanee@smartsensesolutions.com>
2021-02-26 12:02:48 +0530

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+  router: {
+    base: '/nuxtjs_practice_first/'
+  },
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Coding Mania',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: 'Weekly and daily posts about Swift, SwiftUI, Apple Development' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+    '~/plugins/MetaDataWrapper.js'
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
---
+++ pages/about/index.vue
@@ -0,0 +1,30 @@
+<template>
+ <div>
+   <h1>About Page 1</h1>
+   <nuxt />
+ </div>
+</template>
+
+<script>
+export default {
+   head () {
+     return this.greet('About Testing Param Title', `About Testing Description - 1`)
+  },
+  methods: {
+    greet: function (metaTitle, metaDesc) {
+      // `this` inside methods point to the Vue instance 
+      return {
+        title: metaTitle,
+        meta: [{
+          hid: `description`,
+          name: 'description',
+          content: metaDesc
+        }
+        ],
+      }
+      // console.log('Got the thing. ' + metaTitle)
+      
+    }
+  }
+}
+</script>
